c2c4127eaf541738c017329eac7d4040ef8a1df5
student <student@vlab.local>
2022-10-14 14:29:15 +0200

    Initial commit

---
+++ src/Zadanie5/Zadanie.java
@@ -0,0 +1,21 @@
+package Zadanie5;
+
+public class Zadanie {
+
+    public static void main(String[] args) {
+
+        double produkt = 9.99;
+
+        produkt = (produkt*1.23);
+
+        Double sprzedaz = produkt*10000;
+
+        sprzedaz = sprzedaz/1.23;
+
+        System.out.println("Sprzedaz 10000 tego rodzaju produktow (z bez Vat-u) wynosi: ");
+
+        System.out.println(sprzedaz);
+
+    }
+
+}
